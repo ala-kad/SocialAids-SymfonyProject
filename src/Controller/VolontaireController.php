ad5af1e64a63e6a6b86c9d2c5c1308e510eb83d1
ala-kad <kaddechiala@gmail.com>
2022-10-14 15:28:04 +0100

    install and use forms in Volontaire classs

--- src/Controller/VolontaireController.php
+++ src/Controller/VolontaireController.php
@@ -2,50 +2,48 @@
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Types\IntegerType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Entity\Volontaire;
+
+
 class VolontaireController extends AbstractController
 {
     /**
-     * @Route("/SocialAids/volontaire", name="app_volontaire")
+     * @Route("/volontaire", name="app_volontaire")
      */
-    public function index(): Response
+    /*public function index(): Response
     {
         return $this->render('volontaire/index.html.twig', [
             'controller_name' => 'VolontaireController',
         ]);
-    }
-
-    public function create(Request $request):Response{
+    }*/
+    /**
+     * @Route("/volontaire/add", name="app_volontaire")
+     */
+    public function create(Request $request): Response{
         $volontaire = new Volontaire();
 
         $volontaire->setCin("11942595");
         $volontaire->setFirstname("Ala");
         $volontaire->setLastname("Kaddechi");
         $volontaire->setEmail("kaddechiala@gmail.com");
-
-        $form = $this->createFormBuilder($volontaire)
-            ->add("CIN ", IntegerType::class)
-            ->add("First Name", TextType::class)
-            ->add("Last Name", TextType::class)
-            ->add("Email", EmailType::class)
+         $form = $this->createFormBuilder($volontaire)
+            ->add("cin", IntegerType::class)
+            ->add("firstname", TextType::class)
+            ->add("lastname", TextType::class)
+            ->add("email", EmailType::class)
             ->add('save', SubmitType::class, ['label' => 'Create Volontaire'])
             ->getForm();
-
-
-
-
-
-
-
-
-
+        return $this->render('volontaire/index.html.twig', [
+            'controller_name' => 'VolontaireController','form'=>$form->createView()
+        ]);
     }
 }
