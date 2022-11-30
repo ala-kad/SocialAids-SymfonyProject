3f47073a6bb9d55c30336ec74932cd89f9531c98
ala-kad <kaddechiala@gmail.com>
2022-11-30 10:18:14 +0100

    update gitignore file fixing project conflicts

--- src/Entity/Volontaire.php
+++ src/Entity/Volontaire.php
@@ -7,38 +7,35 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass=VolontaireRepository::class)
- */
+#[ORM\Entity(repositoryClass: VolontaireRepository::class)]
+
 
 class Volontaire
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private ?int $id= null;
 
-    /**
-     * @ORM\Column(type="string", length=10)
-     */
+    #[ORM\Column]
     private $cin;
 
-    /**
-     * @ORM\Column(type="string", length=20)
-     */
+    #/**
+     #* @ORM\Column(type="string", length=20)
+    # */
+    #[ORM\Column]
     private $firstname;
 
-    /**
-     * @ORM\Column(type="string", length=20)
-     */
+    #/**
+    # * @ORM\Column(type="string", length=20)
+     #*/
+    #[ORM\Column]
     private $lastname;
 
 
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
+     #* @ORM\Column(type="string", length=50)
+    # */
+    #[ORM\Column]
     private $email;
 
     /**
