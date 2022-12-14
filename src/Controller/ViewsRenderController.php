29d67ba718ef74db056731c22889e481261f50aa
ala-kad <kaddechiala@gmail.com>
2022-12-14 01:36:51 +0100

    render home page

--- src/Controller/ViewsRenderController.php
+++ src/Controller/ViewsRenderController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
-class ViewsRenderController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+
+class ViewsRenderController extends AbstractController
+{
+    /**
+     * @Route("/" name = "home_index")
+     */
+    public function index(){
+        return $this->render("home/home.html.twig");
+    }
 }
